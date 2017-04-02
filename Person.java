18522c52ba850f9199f863372703e0cfdc395416
NotBattleFrog <tigergood@live.com>
2017-04-02 22:05:50 +0700

    comments

--- Person.java
+++ Person.java
@@ -18,13 +18,13 @@ public class Person extends Actor
      static final int west = 1;
      static final int north = 2;
      static final int south = 3;
-     static String high ;
-     static String name;
-     static int highScore = 0;
-     static int score;
-     static int pu;
-     static boolean in;
-     boolean dead;
+     static String high ; // highest score name.
+     static String name; // current player name.
+     static int highScore = 0; // highest score.
+     static int score; // current score.
+     static int pu; // invincibility time;
+     static boolean in; // invincibility.
+     boolean dead; 
      public Person()
      {
             this(0,0,false,false);
@@ -51,9 +51,9 @@ public class Person extends Actor
     
         
     public void Ask(){// To get User's name
-                         while(true){
- String x =Greenfoot.ask("Please input your name? (8 characters maximum )"); 
- if(x.length()<=8){
+                         while(true){ //Will end if the name length is less than or equal 8.
+ String x =Greenfoot.ask("Please input your name? (8 characters maximum )"); //Using the ask method, you will get the player's name.
+ if(x.length()<=8){ //To check the length.
      name =x; 
      break;
     }
@@ -128,53 +128,53 @@ public class Person extends Actor
     {
         int whichWay;
   
-        if (Greenfoot.isKeyDown("right") || Greenfoot.isKeyDown("D")) 
+        if (Greenfoot.isKeyDown("right") || Greenfoot.isKeyDown("D"))  // move to the right if the specific key is pressed.
         {
             whichWay = 0;
-            setImage("personright.png" );
+            setImage("personright.png" ); // set the image to facing right.
             if(getX()==15&&getWorld().getObjectsAt(0,getY(),Rock.class).isEmpty()){ //warp if warpable and there are no rocks in the way.
                  setLocation( 0 , getY());
             }
-     else if (canYouMove(whichWay) == true){
+     else if (canYouMove(whichWay) == true){ // if there are no rocks in the desired direction.
  
                 moveYou(whichWay);}
       
     }
-        if (Greenfoot.isKeyDown("left") || Greenfoot.isKeyDown("A")) 
+        if (Greenfoot.isKeyDown("left") || Greenfoot.isKeyDown("A")) // move to the left if the specific key is pressed.
         {
             whichWay = 1;
-            setImage("personleft.png");
+            setImage("personleft.png");// set the image to facing left.
             
              if(getX()==0&&getWorld().getObjectsAt(16,getY(),Rock.class).isEmpty()){//warp if warpable and there are no rocks in the way.
                  setLocation( 16 , getY());
             }
-     else if (canYouMove(whichWay) == true){
+     else if (canYouMove(whichWay) == true){// if there are no rocks in the desired direction.
  
                 moveYou(whichWay);}
     
         }
-    if (Greenfoot.isKeyDown("up") || Greenfoot.isKeyDown("W")) 
+    if (Greenfoot.isKeyDown("up") || Greenfoot.isKeyDown("W")) // move up if the specific key is pressed.
         {
             whichWay = 2;
-            setImage("personup.png");
+            setImage("personup.png");// set the image to facing up.
          
            
               if(getY()==0&&getWorld().getObjectsAt(getX(),15,Rock.class).isEmpty()){ //warp if warpable and there are no rocks in the way.
                  setLocation( getX(),15);
             }
-     else if (canYouMove(whichWay) == true){
+     else if (canYouMove(whichWay) == true){// if there are no rocks in the desired direction.
  
                 moveYou(whichWay);}
     }
-        if (Greenfoot.isKeyDown("down") || Greenfoot.isKeyDown("S")) 
+        if (Greenfoot.isKeyDown("down") || Greenfoot.isKeyDown("S"))  // move down if the specific key is pressed.
         {
             whichWay = 3;
-            setImage("persondown.png");
+            setImage("persondown.png"); // set the image to facing down.
              
           if(getY()==15&&getWorld().getObjectsAt(getX(),0,Rock.class).isEmpty()){ //warp if warpable and there are no rocks in the way.
                  setLocation( getX(),0);
             }
-     else if (canYouMove(whichWay) == true){
+     else if (canYouMove(whichWay) == true){// if there are no rocks in the desired direction.
  
                 moveYou(whichWay);}
         }
@@ -183,34 +183,34 @@ public class Person extends Actor
     
     
     public void CheckPo(){ // For collecting power ups
-        if(isTouching(Pwr.class)&&!in){
-            Greenfoot.playSound("Go.mp3");
-            in = true;
-            removeTouching(Pwr.class);
+        if(isTouching(Pwr.class)&&!in){ // if this object touches the power up.
+            Greenfoot.playSound("Go.mp3"); // play this sound.
+            in = true; // Activate the invincibility.
+            removeTouching(Pwr.class); // remove the touching power up.
         }
     }
     public void Po(){ //Transparency when pick up power ups
-        if(in){
-            pu++;
-            getImage().setTransparency(100);
+        if(in){ //while being invincible.
+            pu++; // increase the invinciblity time.
+            getImage().setTransparency(100); //make the image transparent.
         }
-        if(pu>=30){
-            in=false;
-            getImage().setTransparency(255);
-            pu = 0;
+        if(pu>=30){ // if the invinciblity time reaches 30.
+            in=false; // Deactivate the invincibility.
+            getImage().setTransparency(255); // set the image to normal transparency
+            pu = 0; // reset the invinciblity time.
         }
     }
     public void checkScore() //check the highest score.
     {
         
    
-     if(score > highScore)
+     if(score > highScore) // if the score is higher than the high score.
     {
-        high =name;
-    highScore = score;
+        high =name; //replace the highest score name with the current name.
+    highScore = score;//replace the highest score with the current score.
     }
-    else if(high==null){
-      high =name;
+    else if(high==null){ // if there is no high score yet.
+      high =name; // set the current name.
     }
 
     }
@@ -219,14 +219,14 @@ public class Person extends Actor
     public void death() // to check if you touch zombie the game will be over
     {
     Actor zombie = getOneObjectAtOffset(0 , 0, Zombie.class);
-    if(zombie != null && isTouching(Zombie.class)&&!in) 
+    if(zombie != null && isTouching(Zombie.class)&&!in) //if this object touches the zombie while invincibility is still deactivate.
     {
            
         dead = true;
-          checkScore();
-    getWorld().removeObject(this);
+          checkScore(); // check the score.
+    getWorld().removeObject(this); // remove this object.
     Greenfoot.playSound("Scream.wav");
-    Greenfoot.setWorld(new ToMainMenu());
+    Greenfoot.setWorld(new ToMainMenu()); // go to the score menu.
     }
     }
     
