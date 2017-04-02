45049e47b0c4e072576f06c903382d852bf7a64f
NotBattleFrog <tigergood@live.com>
2017-04-02 21:47:32 +0700

    a method and comments

--- MineEasy.java
+++ MineEasy.java
@@ -13,32 +13,36 @@ public class MineEasy extends World
      * Constructor for objects of class MineEasy.
      * 
      */
-    int  time = 90;
-    static int level;
+    int  time = 90; // time limit.
+    static int level; // the current level.
     
     int timer = 40;
     int spawn = 0;
-        int spn;
+        int spn; // delay between spawning a power up.
     public MineEasy(int level)
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        // Create a new world with 16x16 cells with a cell size of 32x32 pixels.
         super(16, 16, 32); 
         
         this.level = level; // get the level.
         deterLe(); // To determind the level and create objects respond to the level.
-        addObject(new TryA() , 2 , 0);
-        addObject(new MainMenu() , 12 , 1);
+        addObject(new TryA() , 2 , 0); // add a restart button.
+        addObject(new MainMenu() , 12 , 1); // add a main menu button.
            
     }
 
     public void act()
     {
-         showText("Time : " + time , 8 , 0);
-         showText("Score : " + Person.score , 8 , 2);
-          showText("Invincibility : " + Person.in , 8 , 4);
-   spwnPo();
+         showText("Time : " + time , 8 , 0); // Show the time.
+         showText("Score : " + Person.score , 8 , 2); // Show the player's score.
+          showText("Invincibility : " + Person.in , 8 , 4); // Show the current state of invincibility.
+   spwnPo(); // see spwnPo method.
           spn++;
-        if(timer == 0)
+    spwner();
+    timer -=2 ;
+}
+public void spwner(){
+      if(timer == 0)
         {
             spawn++;
             time --;
@@ -66,10 +70,9 @@ public class MineEasy extends World
       
     timer = 40;
     }
-    timer -=2 ;
 }
 public void deterLe(){ //check which level is selected and create the needed objects .
-     if(level==0){
+     if(level==0){ //Easy Mode
                for(int i = 1; i <=15; i++)
         {
         addObject(new Rock() , 0, i);
@@ -99,7 +102,7 @@ public void deterLe(){ //check which level is selected and create the needed obj
         addObject(new Zombie() , 8 , 8);
         spn=50;
     }
-    if(level==1){
+    if(level==1){ // Normal mode.
         addObject(new Person() , 1 , 14);
 
         addObject(new Rock(), 9 , 15 );
@@ -132,7 +135,7 @@ public void deterLe(){ //check which level is selected and create the needed obj
         addObject(new Zombie() , 14 , 9);
           spn=70; 
     }
-    if(level==2){
+    if(level==2){ // Hard mode.
          addObject(new Person() , 1 , 14);
         addObject(new Rock() , 1 , 15);
         addObject(new Rock() , 8 , 15);
@@ -190,13 +193,14 @@ public void deterLe(){ //check which level is selected and create the needed obj
   public void spwnPo(){ //To spawn power up
     int x,y;
             
-                x = Greenfoot.getRandomNumber(10)+1;
-                y = Greenfoot.getRandomNumber(10)+1;
+                x = Greenfoot.getRandomNumber(10)+1; // get the location x randomly.
+                y = Greenfoot.getRandomNumber(10)+1; // get the location y randomly.
+                // if that location doesn't have a rock,a goldbar,a power up, and a person. Spawn a power up there.
                     if(getObjectsAt(x , y ,(Rock.class)).size() == 0&&getObjectsAt(x , y ,(Goldbar.class)).size() == 0&&getObjectsAt(x , y ,(Pwr.class)).size() == 0&&getObjectsAt(x , y ,(Person.class)).size() == 0)
             {
-                if(spn>=50){
-            addObject(new Pwr(), x, y);
-            spn=0;
+                if(spn>=50){ 
+            addObject(new Pwr(), x, y); // spawn a power up.
+            spn=0; // reset the delay.
         }
         }
 }  
