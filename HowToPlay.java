9cc31c9f8ded43d88d47413f9c5041ac2f2c4630
NotBattleFrog <tigergood@live.com>
2017-04-02 21:36:20 +0700

    add comments

--- HowToPlay.java
+++ HowToPlay.java
@@ -15,12 +15,12 @@ public class HowToPlay extends World
      */
     public HowToPlay()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        // Create a new world with 700x450 cells with a cell size of 1x1 pixels.
         super(700, 450, 1); 
-        addObject(new HowTo() , 340 , 180);
-        addObject(new NextPlay() , 594 , 399);
+        addObject(new HowTo() , 340 , 180);// add the HowTo object to the world.(See more at HowTo class)
+        addObject(new NextPlay() , 594 , 399); // add the NeXtPlay object to the world.(See more at NextPlay class)
         showText("1.Collect the gold bars to get score" + 
         "\n                  2.Beware zombies do not let them touch you." + 
-        "\n                                    3.Using W A S D or UP , RIGHT , DOWN LEFT to control " , getHeight()/2 , 300);
+        "\n                                    3.Using W A S D or UP , RIGHT , DOWN LEFT to control " , getHeight()/2 , 300); // show instruction texts.
     }
 }
