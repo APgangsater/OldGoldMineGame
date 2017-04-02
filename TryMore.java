8238891bd7fe54ba75cd5ae4010b0191dfc4deab
NotBattleFrog <tigergood@live.com>
2017-04-02 22:12:15 +0700

    adding comments and a method

--- TryMore.java
+++ TryMore.java
@@ -12,16 +12,21 @@ public class TryMore extends Actor
      * Act - do whatever the TryMore wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+     public TryMore(){
+      setImage(new GreenfootImage("Try Again?" , 50 , Color.WHITE , Color.BLACK));
+        getImage();
+     }
     public void act() 
     {
         // Add your action code here.
-        setImage(new GreenfootImage("Try Again?" , 50 , Color.WHITE , Color.BLACK));
-        getImage();
-        if(Greenfoot.mouseClicked(this))//To check if the mouse clicked 
+       check();
+    
+    }  
+    public void check(){
+          if(Greenfoot.mouseClicked(this))//To check if the mouse clicked 
         {
         Greenfoot.playSound("Clicking.wav");   
         Greenfoot.setWorld(new SelectMode());
-        Person.score = 0;
-    }    
-    }    
+    }  
+    }
 }
