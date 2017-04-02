f0208b0bdde71433d045946b7861e25be4ea30fa
NotBattleFrog <tigergood@live.com>
2017-04-02 21:32:45 +0700

    add comments

--- Goldbar.java
+++ Goldbar.java
@@ -12,15 +12,11 @@ public class Goldbar extends Actor
      * Act - do whatever the Goldbar wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public Goldbar()
+    public Goldbar() //Constuctor for making the gold bar image.
     {
-    GreenfootImage image = getImage();
-    setImage(image);
-    image.scale(30 , 30);
+    GreenfootImage image = getImage(); // get the image and store it in "image".
+    setImage(image);  // set the "image".
+    image.scale(30 , 30); // scale the size of the image.
     }
-    public void act() 
-    {
-        // Add your action code here.
-    
-    }    
+   
 }
