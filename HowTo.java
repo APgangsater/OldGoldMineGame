a549edf2ee65704a0e55cd5820d02e53cd12ee57
NotBattleFrog <tigergood@live.com>
2017-04-02 21:34:35 +0700

    add comments

--- HowTo.java
+++ HowTo.java
@@ -12,10 +12,10 @@ public class HowTo extends Actor
      * Act - do whatever the HowTo wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act() 
+    public HowTo() 
     {
         // Add your action code here.
-        setImage(new GreenfootImage("HOW TO PLAY" , 100 , Color.WHITE , Color.BLACK));
+        setImage(new GreenfootImage("HOW TO PLAY" , 100 , Color.WHITE , Color.BLACK)); // create a new image with the word "HOW TO PLAY" in white and the Bg in black.
         getImage();
     }    
 }
